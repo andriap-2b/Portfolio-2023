8227d252e2c13dc944f4ed23b5f6b5ae79eb257f
Andria Piercecchi <andriapiercecchi@macbook-pro-de-andria.home>
2023-05-29 11:50:08 +0200

    ajout du portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*-- typing animation --*/
+var typed = new Typed(".typing", {
+    strings:["", "Développement Web", "Design Graphique","Montage Video"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
