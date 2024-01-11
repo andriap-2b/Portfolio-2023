30fac74f57214c988c937ca8dbaf11562958f134
andriap-2b <97974037+andriap-2b@users.noreply.github.com>
2024-01-11 17:49:54 +0100

    Update script.js
    2024

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*-- typing animation --*/
 var typed = new Typed(".typing", {
-    strings:["", "Développement Web", "Design Graphique","Montage Video"],
+    strings:["", "la photographie", "le design graphique","le montage video"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
